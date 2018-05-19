ffd446738dcac455d860915c49416938ebdf3a90
Jesse Almanrode <jesse@almanrode.com>
2018-05-18 18:18:20 -0600

    Adding 60_docker.sh source

---
+++ sources/60_docker.sh
@@ -0,0 +1,12 @@
+### Bash Profile: Docker Additions
+### Author: Jesse Almanrode (https://about.me/JesseAlmanrode)
+### License: GNU GPLv3 (https://choosealicense.com/licenses/gpl-3.0/)
+### Aliases and functions which load if you have docker installed
+
+if [ -z $(which docker 2> /dev/null) ]; then
+	echo "docker is not installed. Not loading source.";
+	return 1;
+fi
+
+alias docker_clean_images='docker rmi $(docker images -a --filter=dangling=true -q)';
+alias docker_clean_ps='docker rm $(docker ps --filter=status=exited --filter=status=created -q)';
