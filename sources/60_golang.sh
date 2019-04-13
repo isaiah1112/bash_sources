de2baf087660d294bce1c7d6253a642380c33f7a
Jesse Almanrode <jesse@almanrode.com>
2019-04-13 10:38:22 -0600

    Added new source for GoLang

---
+++ sources/60_golang.sh
@@ -0,0 +1,10 @@
+### Bash Profile: GoLang Additions
+### Author: Jesse Almanrode (https://about.me/JesseAlmanrode)
+### License: GNU GPLv3 (https://choosealicense.com/licenses/gpl-3.0/)
+### Aliases and functions which load if you have go installed
+
+if [ -z $(which go 2> /dev/null) ]; then
+	echo "golang is not installed. Not loading source.";
+	return 1;
+fi
+export PATH=$PATH:$HOME/go/bin;
