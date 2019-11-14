ed49c9f7777fe6d7be2ec68b9e636e50cc1938ce
Jesse Almanrode <jesse@almanrode.com>
2019-11-14 08:48:37 -0700

    Added mkcaption function.  Updated mkcontactsheet function.

--- sources/60_imagemagick.sh
+++ sources/60_imagemagick.sh
@@ -4,9 +4,25 @@
 ### Aliases and functions which load if you have ImageMagick installed
 
 
-if [ -z $(which montage 2> /dev/null) ]; then
-	echo "montage is not installed. Not loading source.";
+if [ -z $(which montage 2> /dev/null) -a -z $(which convert 2> /dev/null) ]; then
+	echo "ImageMagick is not installed. Not loading source.";
 	return 1;
 fi
 
-alias mkcontactsheet='montage -geometry +5+5 -tile 3x5 -frame 5';
+function mkcontactsheet() {
+	if [ -z "$1" -o "$1" == "--help" -o $# -ne 2 ]; then
+			echo 'USAGE: mkcontactsheet <input> <output>';
+			return 0;
+	fi
+	montage -geometry +5+5 -tile 3x5 -frame 5 "$1" "$2";
+	echo "Wrote file: $2";
+}
+
+function mkcaption() {
+	if [ -z "$1" -o "$1" == "--help" -o $# -ne 2 ]; then
+			echo 'USAGE: mkcaption <str> <file>';
+			return 0;
+	fi
+	convert -size 1024x160 -background white -pointsize 25 -fill black -gravity NorthWest caption:"$1" -flatten "$2";
+	echo "Wrote file: $2"
+}
