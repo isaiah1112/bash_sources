92db2a0b3a6981e7752ae859d039ba7eaab7838e
Jesse Almanrode <jesse@almanrode.com>
2019-11-14 17:14:56 -0700

    Fixed bug when bit_rate is N/A in ffinfo

--- sources/60_ffmpeg.sh
+++ sources/60_ffmpeg.sh
@@ -30,7 +30,9 @@ function ffinfo () {
 	echo Filename: "$1";
 	echo Format: $(echo "$video_stats" | grep -Eo 'h264|h265|mjpeg|wmv3');
 	echo Resolution: $(echo "$video_stats" | grep 'height' | grep -Eo '\d+')p;
-	echo Bitrate: $(( $(echo "$video_stats" | grep bit_rate | grep -Eo '\d+') / 1000))kb/s;
+	if [ "$(echo "$video_stats" | grep bit_rate)" = "N/A" ]; then
+		echo Bitrate: $(( $(echo "$video_stats" | grep bit_rate | grep -Eo '\d+') / 1000))kb/s;
+	fi
 	echo Filesize: $(ls -h "$1" | awk '{print $5}');
 }
 
