6164c3a8ee239e5ffa129be436a726a5609b94ed
Jesse Almanrode <jesse@almanrode.com>
2020-08-25 10:06:58 -0600

    Added mktimelapse and mkvlapse functions to 60_ffmpeg.sh

--- sources/60_ffmpeg.sh
+++ sources/60_ffmpeg.sh
@@ -21,6 +21,35 @@ function m3u8_download() {
 	ffmpeg -hide_banner -i "$1" -c copy -bsf:a aac_adtstoasc "$2";
 }
 
+# function to create a timelapse video from a list of images
+function mktimelapse() {
+	if [ -z "$1" -o "$1" == "--help" ]; then
+		echo "USAGE: mktimelapse <files...> [fps] [preset]";
+		return 0;
+	fi
+	if [ -n "$2" ]; then
+		fps=$2;
+	else
+		fps=15;
+	fi
+	if [ -n "$3" ]; then
+		preset=$3;
+	else
+		preset='medium';
+	fi
+	ffmpeg -r $fps -pattern_type glob -i "$1" -vf "scale=1920:-1" -vcodec libx264 -preset $preset -crf 15 -pix_fmt yuv420p ./timelapse.mp4;
+}
+
+# create a timelapse video from a video file
+function mkvlapse() {
+	if [ -z "$1" -o "$1" == "--help"]; then
+		echo "USAGE: mkvlapse <file>";
+		return 0;
+	fi
+	name=$(echo "$1" | rev | cut -d '.' -f2- | rev);
+	ffmpeg -i "$1" -filter:v "setpts=0.5*PTS" -an ${name}_timelapse.mp4;
+}
+
 # function for printing specific details about a video in a specific show_format
 function ffinfo () {
 	if [ -z "$1" -o "$1" == "--help" ]; then
