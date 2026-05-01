49e0123d260b4e521491d78a3e186570995ecf06
Jesse Almanrode <jesse@almanrode.com>
2026-05-01 10:30:46 -0600

    Copilot updates to `60_docker.sh`

--- bash_sources/60_docker.sh
+++ bash_sources/60_docker.sh
@@ -3,10 +3,27 @@
 ### License: GNU GPLv3 (https://choosealicense.com/licenses/gpl-3.0/)
 ### Aliases and functions which load if you have docker installed
 
-if [ -z $(which docker 2> /dev/null) ]; then
-	echo "docker is not installed. Not loading source.";
-	return 1;
+if ! command -v docker &>/dev/null; then
+    echo "Docker is not installed. Not loading source."
+    return 1
 fi
 
-alias drmi="docker rmi $(docker images -a --filter=dangling=true -q)";
-alias drmps="docker rm $(docker ps --filter=status=exited --filter=status=created -q)";
+# Remove dangling images
+drmi() {
+    docker image prune -f
+}
+
+# Remove stopped containers
+drmps() {
+    docker container prune -f
+}
+
+# Common docker shortcuts
+alias dps='docker ps'
+alias dpsa='docker ps -a'
+alias di='docker images'
+alias dex='docker exec -it'
+alias dlog='docker logs -f'
+alias dstop='docker stop'
+alias dstart='docker start'
+alias dstat='docker stats'
