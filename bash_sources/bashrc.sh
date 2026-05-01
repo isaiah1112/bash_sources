bc5a2057922c4b33eda989a7a92b6ee43bbc3a59
Jesse Almanrode <jesse@almanrode.com>
2026-05-01 10:24:16 -0600

    Copilot improvements to `bashrc.sh`
    Glob pattern instead of find | grep — faster and simpler
    Proper quoting — prevents issues with spaces in paths
    Default values for arguments — prevents unbound variable errors with set -u
    Removed $HOME in function — use ${HOME} or "$HOME" consistently

--- bash_sources/bashrc.sh
+++ bash_sources/bashrc.sh
@@ -1,43 +1,47 @@
 ### Author: Jesse Almanrode (jesse@almanrode.com)
 ### License: GNU GPLv3 (https://choosealicense.com/licenses/gpl-3.0/)
 
-## Load files and links in $HOME/.sources.d/ that end in .sh
-BASH_SOURCES="$HOME/.bash_sources.d";
+## Load files in $HOME/.bash_sources.d/ that end in .sh
+BASH_SOURCES="${HOME}/.bash_sources.d"
 
-if [ -d $BASH_SOURCES ]; then
-	for f in $(find $BASH_SOURCES | grep ".sh$" | sort); do
-		source $f;
-	done
+if [[ -d "$BASH_SOURCES" ]]; then
+    for f in "$BASH_SOURCES"/*.sh; do
+        [[ -e "$f" ]] && source "$f"
+    done
 else
-	echo "Please run 'install.sh bashrc'";
-	return 1;
+    echo "Please run 'install.sh bashrc'"
+    return 1
 fi
 
 ## Functions that make the loader tick
-function profile() {
-case $1 in
-	edit)
-		if [ -z $2 ]; then
-    		echo 'profile edit <source>';
-    	else
-      		vi $BASH_SOURCES/$2;
-      		source $HOME/.bash_profile;
-    	fi
-	;;
-	view)
-		if [ -z $2 ]; then
-    		echo 'profile view <source>';
-    	else
-      		less $BASH_SOURCES/$2;
-    	fi
-	;;
-	reload)
-		source $HOME/.bash_profile;
-	;;
-	list)
-		ls -1 $BASH_SOURCES/;
-	;;
-	*)
-		echo 'profile [edit|reload|view|list]';
-esac
+profile() {
+    local cmd="${1:-}"
+    local arg="${2:-}"
+
+    case "$cmd" in
+        edit)
+            if [[ -z "$arg" ]]; then
+                echo 'profile edit <source>'
+            else
+                vi "$BASH_SOURCES/$arg"
+                source "${HOME}/.bash_profile"
+            fi
+            ;;
+        view)
+            if [[ -z "$arg" ]]; then
+                echo 'profile view <source>'
+            else
+                less "$BASH_SOURCES/$arg"
+            fi
+            ;;
+        reload)
+            source "${HOME}/.bash_profile"
+            ;;
+        list)
+            ls -1 "$BASH_SOURCES/"
+            ;;
+        *)
+            echo 'profile [edit|reload|view|list]'
+            ;;
+    esac
 }
